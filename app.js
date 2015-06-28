04ce77a9a002088bf2f84c2030137d9069080d4a
Fang Jie <fangjie1100@126.com>
2015-06-28 23:40:54 +0800

    inport semantic-ui ,index page inport template

--- app.js
+++ app.js
@@ -24,6 +24,7 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/bower_components', express.static(path.join(__dirname, 'bower_components')));
 
 app.use('/', routes);
 app.use('/users', users);
